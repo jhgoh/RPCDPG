43f2c915201d229697956dd325189a15f8af54da
Junghwan John Goh <jhgoh@cern.ch>
2017-05-18 15:25:48 +0200

    filename

---
+++ Configuration/python/customise_rpcRedigi_cff.py
@@ -0,0 +1,3 @@
+import FWCore.ParameterSet.Config as cms
+
+from SimMuon.RPCDigitizer.customizeRPCDigi import customise_rpcRedigi
--- Configuration/python/rpcRedigi_cff.py
+++
@@ -1,4 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from SimMuon.RPCDigitizer.customiseRPCDigi import customise_rpcRedigi
-
