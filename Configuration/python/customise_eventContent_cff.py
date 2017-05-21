b7d98cf0da438f77176df42f467b27658c442355
Junghwan John Goh <jhgoh@cern.ch>
2017-05-21 16:04:57 +0200

    save muon and RPC stuffs only

--- Configuration/python/customise_eventContent_cff.py
+++ Configuration/python/customise_eventContent_cff.py
@@ -3,9 +3,13 @@ import FWCore.ParameterSet.Config as cms
 def customise_eventContent(process):
     process.setName_('RERECO')
 
-    process.AODSIMoutput.outputCommands += [
-        "keep *_simMuonRPCDigis_*_*",
+    process.AODSIMoutput.outputCommands = [
+        "drop *",
+        "keep *_genParticles_*_*",
         "keep *_g4SimHits_MuonRPCHits_*",
+        "keep *_simMuonRPCDigis_*_*",
+        "keep *_muons__*",
+        "keep *_rpcRecHits_*_*",
     ]
 
     return process
