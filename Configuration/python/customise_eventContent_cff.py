a717574c98408d41c4f869c5bd4245b0d02f3f35
Junghwan John Goh <jhgoh@cern.ch>
2017-06-22 15:07:05 +0200

    more contents for the HSCP tree

--- Configuration/python/customise_eventContent_cff.py
+++ Configuration/python/customise_eventContent_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 def customise_eventContent(process):
-    process.setName_('RERECO')
+    process.setName_('reRECO')
 
     process.AODSIMoutput.outputCommands = [
         "drop *",
@@ -9,8 +9,29 @@ def customise_eventContent(process):
         "keep *_g4SimHits_MuonRPCHits_*",
         "keep *_simMuonRPCDigis_*_*",
         "keep *_simMuonRPCReDigis_*_*",
+
         "keep *_muons__*",
+
         "keep *_rpcRecHits_*_*",
+        'keep *_gemRecHits_*_*',
+        'keep *_me0RecHits_*_*',
+
+        'keep *_*_MergedTrackTruth_*',
+
+        'keep recoTracks_generalTracks_*_*',
+        'keep recoTrackExtras_generalTracks_*_*',
+        'keep TrackingRecHitsOwned_generalTracks_*_*',
+
+        'keep recoTracks_standAloneMuons_*_*',
+        'keep recoTrackExtras_standAloneMuons_*_*',
+        'keep TrackingRecHitsOwned_standAloneMuons_*_*',
+
+        'keep recoTracks_globalMuons_*_*',
+        'keep recoTrackExtras_globalMuons_*_*',
+        'keep TrackingRecHitsOwned_globalMuons_*_*',
+        'keep recoMuonTrackLinkss_globalMuons_*_*',
+
+        'keep  *_offlinePrimaryVertices__*',
     ]
 
     return process
